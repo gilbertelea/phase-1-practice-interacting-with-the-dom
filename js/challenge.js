45f9982928691de83294db10fd7227aac24657a7
GILBERT ELEAZAR BIBOUM BI NZOGUE <eleazarbiboum@gmail.com>
2022-05-20 18:44:06 -0400

    assignment completed

---
+++ js/challenge.js
@@ -0,0 +1,214 @@
+
+
+let time = 0;
+let status = true;
+
+const startTimer = () => {
+  time++;
+  document.getElementById('counter').innerHTML = time;
+};
+
+timer = setInterval(startTimer, 1000);
+
+const actionTimer = () => {
+  if (status) {
+    clearInterval(timer);
+    document.getElementById('pause').innerHTML = 'resume';
+  } else {
+    timer = setInterval(startTimer, 1000);
+    document.getElementById('pause').innerHTML = 'pause';
+  }
+  status = !status;
+};
+
+let minus = document.getElementById('minus');
+minus.addEventListener('click', () => {
+  time--;
+  document.getElementById('counter').innerHTML = time;
+});
+
+let plus = document.getElementById('plus');
+plus.addEventListener('click', () => {
+  time++;
+  document.getElementById('counter').innerHTML = time;
+});
+
+let heart = document.getElementById('heart');
+heart.addEventListener('click', () =>{
+let Li = document.createElement('li')
+//append p into ul tag with class "likes"
+document.querySelector('.likes').appendChild(Li)
+//create a and target his value       
+let a = document.getElementById("counter")
+// console.log(a.textContent + `  has been liked <span>" +1 + "</span> times`);
+let b = a.textContent + " has been liked 1 time"
+Li.textContent = b
+
+});
+
+
+let form = document.querySelector('form');
+form.addEventListener('submit', e =>{
+    e.preventDefault()
+    Handle(e.target.comment_input.value)
+    form.reset()
+})
+
+function Handle(todo){
+    let p = document.createElement('p')
+    p.textContent = todo
+    document.querySelector('#list').appendChild(p)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function _toConsumableArray(a) {
+//     if (Array.isArray(a)) {
+//         for (var b = 0, c = Array(a.length); b < a.length; b++)
+//             c[b] = a[b];
+//         return c
+//     }
+//     return Array.from(a)
+// }
+// var playing = !0
+//   , timer = function() {
+//     return setInterval(function() {
+//         var a = document.getElementById("counter")
+//           , b = parseInt(a.innerText);
+//         a.innerText = b + 1
+//     }, 1e3)
+// }
+//    interval = timer()
+//    minus = document.getElementById("minus")
+//   plus = document.getElementById("plus")
+//    heart = document.getElementById("heart")
+//    pause = document.getElementById("pause")
+//   form = document.getElementsByTagName("form")[0];
+
+// minus.addEventListener("click", function() {
+//     var a = document.getElementById("counter")
+//       , b = parseInt(a.innerText);
+//     a.innerText = b - 1
+// }),
+// plus.addEventListener("click", function() {
+//     var a = document.getElementById("counter")
+//       , b = parseInt(a.innerText);
+//     a.innerText = b + 1
+// }),
+// heart.addEventListener("click", function() {
+//     var a = document.getElementById("counter")
+//       , b = parseInt(a.innerText)
+//       , c = document.querySelector(".likes")
+//       , d = void 0;
+//     if ([].concat(_toConsumableArray(c.children)).map(function(a) {
+//         return parseInt(a.dataset.num)
+//     }).includes(b)) {
+//         d = document.querySelector('[data-num="' + b + '"]');
+//         var e = parseInt(d.children[0].innerText);
+//         d.innerHTML = b + " has been liked <span>" + (e + 1) + "</span> times"
+//     } else
+//         (d = document.createElement("li")).setAttribute("data-num", b),
+//         d.innerHTML = b + " has been liked <span>1</span> time",
+//         c.appendChild(d)
+// }),
+// pause.addEventListener("click", function() {
+//     playing ? (playing = !1,
+//     clearInterval(interval),
+//     this.innerText = "resume") : (playing = !0,
+//     interval = timer(),
+//     this.innerText = "pause"),
+//     [].concat(_toConsumableArray(document.getElementsByTagName("button"))).forEach(function(a) {
+//         "pause" !== a.id && (a.disabled = !playing)
+//     })
+// }),
+// form.addEventListener("submit", function(a) {
+//     a.preventDefault();
+//     var b = this.children[0]
+//       , c = b.value;
+//     b.value = "";
+//     var d = document.querySelector(".comments")
+//       , e = document.createElement("p");
+//     e.innerText = c,
+//     d.appendChild(e)
+// });
